62d13a6450011e0893490454f5cc344a9adb8b1b
lsminter <lsminteris@gmail.com>
2017-09-25 16:03:36 -0700

    added correct files

--- script.js
+++ script.js
@@ -14,4 +14,13 @@ const emailObservable = clickObservable
 // mergeMap = map ... + ... mergeAll
 
 emailObservable
-  .subscribe(email => console.log(email));
+  .subscribe(email => console.log(email) || displayInPreview(email));
+
+  
+// display in plunker preview
+function displayInPreview(string) {
+  var newDiv = document.createElement("div"); 
+  var newContent = document.createTextNode(string); 
+  newDiv.appendChild(newContent);
+  document.body.appendChild(newDiv)
+}
